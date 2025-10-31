c921f081e709dbd703e00ba1039a1625813a1dff
Lucas Cruz <lucasdearaujocruz@gmail.com>
2025-10-31 16:18:26 -0600

    feat: add mcts agent with hpo example

---
+++ examples/doublegoright_mcts.py
@@ -0,0 +1,66 @@
+import jax
+from goright.jax.env import GoRightJaxEnv, EnvParams
+
+from rl_research.agents import MCTSAgent, MCTSAgentParams, goright_expectation_model
+from rl_research.experiment import run_experiment, log_experiment, ExperimentParams
+
+
+def main():
+    agent_name = "qlearning_random_walk"
+    experiment_name = "doublegoright"
+    rng = jax.random.PRNGKey(0)
+
+    env_params = EnvParams(
+        length=21,
+        num_indicators=2,
+        num_actions=2,
+        first_checkpoint=10,
+        first_reward=3.0,
+        second_checkpoint=20,
+        second_reward=6.0,
+        is_partially_obs=True,
+        mapping="default",
+    )
+    env = GoRightJaxEnv(
+        env_params
+    )
+
+    agent_params = MCTSAgentParams(
+        num_states=env.env.observation_space.n,
+        num_actions=env.env.action_space.n,
+        discount=0.9,
+        initial_value=0,
+        num_simulations=16,
+        max_depth=10,
+        exploration_constant=100,
+        dynamics_model=goright_expectation_model(is_partially_obs=env.env.params.is_partially_obs),
+    )
+    agent = MCTSAgent(params=agent_params)
+
+    experiment_params = ExperimentParams(
+        num_seeds=30,
+        total_train_episodes=600,
+        episode_length=500,
+        eval_every=1,
+        num_eval_episodes=1,
+    )
+    results = run_experiment(
+        env=env,
+        agent=agent,
+        rng=rng,
+        params=experiment_params
+    )
+
+    log_experiment(
+        experiment_name=experiment_name,
+        parent_run_name=agent_name,
+        agent_name=agent_name,
+        agent_params=agent_params,
+        experiment_params=experiment_params,
+        env_params=env_params,
+        experiment_results=results,
+    )
+
+
+if __name__ == "__main__":
+    main()
